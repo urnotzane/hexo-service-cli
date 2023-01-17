1600a13f39c319d062119612bd1e01b1f2b0c3b7
kaylezang <kaylezang@tencent.com>
2023-01-17 15:48:40 +0800

    fix: 脚本执行错误

--- scripts/set_authorized_key.sh
+++ scripts/set_authorized_key.sh
@@ -2,9 +2,9 @@
 
 su $username <<EOF
   echo -n "$log_prefix 请输入ssh公钥："
-  read id_rsa_pub
-  if [ $id_rsa_pub ]
+  read id_rsa_pub;
+  if [ $id_rsa_pub ];
   then
-      echo $id_rsa_pub >> /home/$username/.ssh/authorized_keys
-  fi
+      echo $id_rsa_pub >> /home/$username/.ssh/authorized_keys;
+  fi;
 EOF
