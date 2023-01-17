e678eec260ece2b4317767dfbeac19d704d52f6c
kaylezang <kaylezang@tencent.com>
2023-01-17 14:53:20 +0800

    fix: 修改创建用户脚本的地址

--- install.sh
+++ install.sh
@@ -11,6 +11,21 @@ export blog_dir="/var/blog"
 # 网站文件地址
 export username=""
 
+echo -n "$log_prefix 请输入要创建的Linux用户名: "
+read username
+export username=$username
+
+# 创建用户
+useradd $username -m -d /home/$username -s /bin/bash
+if [ $? -eq 0 ]
+then
+  echo "$log_prefix 已创建用户$username!"
+fi
+
+echo "$log_prefix 为用户$username设置密码"
+# 设置密码
+passwd $username
+
 sh +x ./scripts/install_persional_git.sh
 
 sh +x ./scripts/init_hexo_git.sh
--- scripts/install_persional_git.sh
+++ scripts/install_persional_git.sh
@@ -8,21 +8,6 @@ sudo apt update -y
 
 sudo apt install git -y
 
-echo -n "$log_prefix 请输入要创建的Linux用户名: "
-read username
-export username=$username
-
-# 创建用户
-useradd $username -m -d /home/$username -s /bin/bash
-if [ $? -eq 0 ]
-then
-  echo "$log_prefix 已创建用户$username!"
-fi
-
-echo "$log_prefix 为用户$username设置密码"
-# 设置密码
-passwd $username
-
 su $username <<EOF
 cd ~
 mkdir .ssh
